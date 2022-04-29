568fd83f34bcdc9ead69f15b8933064414546799
Michael Behrns-Miller [cast] <m@thedigitalmachine.com>
2022-04-29 09:36:55 -0400

    Updated git-sync-cmd documentation

--- git-sync-cmd.js
+++ git-sync-cmd.js
@@ -10,8 +10,25 @@ import { parse_tag_parameters, git_sync } from './version-control.js';
 // Usage:
 //     git-sync [--major|--minor] msg msg "msg with these [-?&|] should be quoted" ...
 //
-// NOTE: The default "simple" git-sync command has no STAMP callback.
-// See rs-sync-cmd.js for an example of how to make your own improved sync command.
+// The default "simple" git-sync command has no STAMP callback.
+// The default "npm" git-sync command can automatically publish, like this:
+//
+//      import * as gs from '@moodboom/git-semver';
+//      import { run_command_sync_to_console } from './run-utils.js';
+//      const tag_params = gs.parse_tag_parameters(process.argv);
+//      const stampFunction = (err, version) => {
+//        if (err) throw err; // Check for the error and throw if it exists.
+//        gs.npm_update_version(version);
+//      };
+//      var changes = gs.git_changes(process.cwd());
+//      gs.git_sync(process.cwd(),tag_params,stampFunction);
+//      if (changes)
+//      {
+//          // There were changes, so let's publish now.
+//          run_command_sync_to_console('npm publish');
+//      }
+//
+// See git-semver-sync-cmd.js for an example of how to make your own custom sync command.
 
 var tag_params = parse_tag_parameters(process.argv);
 git_sync(process.cwd(),tag_params);
